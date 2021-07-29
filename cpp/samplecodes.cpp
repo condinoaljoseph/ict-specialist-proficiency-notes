b4f939583de0ebab3ec0105ae3c809c603a6f7e9
whatoplay <amandaylanan14@gmail.com>
2021-07-29 22:04:30 +0800

    update c++

--- cpp/samplecodes.cpp
+++ cpp/samplecodes.cpp
@@ -105,3 +105,28 @@ int main() {
   }
  
 }
+
+
+/*
+Let's the user input a number then add the all the numbers.
+*/
+#include <iostream>
+
+int main() {
+  
+  int num = 0;
+  int sum = 0;
+  
+  std::cout << "Enter a number: ";
+  std::cin >> num;
+  
+  for (int i = 1; i <= num; i++) {
+    
+    sum = sum + i;
+    std::cout << i << " ";
+    
+  }
+  
+  std::cout << "Sum: " << sum << "\n";
+  
+}
